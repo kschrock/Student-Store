850029e2a7e40d7cb5ac67d381b3e9266d22ca2b
Kordell Schrock <44933349+kschrock@users.noreply.github.com>
2021-06-18 01:00:41 -0500

    Added Backend Cart Logic.

--- react-node-app/models/store.js
+++ react-node-app/models/store.js
@@ -9,10 +9,24 @@ class Store {
         return items
       }
 
+      static async listCart() {
+        // list all items in the cart array
+        const items = storage.get("cart").value()
+        return items
+      }
+
     static async fetchItemById(itemId) {
         // fetch a single transaction
         const item = storage
         .get("products")
+        .find({ id: Number(itemId) })
+        .value()
+        return item
+      }
+      static async fetchItemByIdCart(itemId) {
+        // fetch a single transaction
+        const item = storage
+        .get("cart")
         .find({ id: Number(itemId) })
         .value()
         return item
@@ -32,7 +46,7 @@ class Store {
 
         const items = await Store.listItems()
         const itemId = items.length + 1
-
+        item.itemID = item.id
         const newItem = { id: itemId, ...item }
 
         storage.get("products").push(newItem).write()
@@ -45,6 +59,43 @@ class Store {
             //no need to throw error.
             storage.get("products").remove({id: Number(givenId)}).write();
 
+        }
+
+        static async deleteItemFromCart(givenId) {
+            //already checked if item exits before this call.
+            //no need to throw error.
+            storage.get("cart").remove({itemId: Number(givenId)}).write();
+
+        }
+
+        static async addToCart(item) {
+            // create a new item
+    
+            if (!item) {
+                throw new BadRequestError(`No item sent.`)
+            }
+            const requiredFields = ["id", "name", "price", "quantity"]
+            requiredFields.forEach((field) => {
+                if (!item[field] && item[field] !== 0) {
+                throw new BadRequestError(`Field: "${field}" is required in transaction`)
+                }
+            })
+    
+             const items = await Store.listCart()
+             const itemId = items.length + 1
+    
+            const newItem = { itemId: itemId, ...item }
+    
+            storage.get("cart").push(newItem).write()
+    
+            return newItem
+            }
+
+        static async updateQuantity(item) {
+            const itemId = item.id
+            storage.get("cart").remove({id: Number(itemId)}).write();
+            storage.get("cart").push(item).write()
+            return item
         }
         
 }
--- react-node-app/routes/store.js
+++ react-node-app/routes/store.js
@@ -2,6 +2,7 @@ const express = require("express")
 const Store = require("../models/store")
 const { NotFoundError } = require("../utils/errors")
 const router = express.Router()
+const cloneDeep = require('lodash.clonedeep')
 
 
 // list all items
@@ -42,6 +43,7 @@ router.delete("/delete/:id", async (req, res, next) => {
         next(err)
       }
   })
+  
 
   // create new item
 router.post("/createItem", async (req, res, next) => {
@@ -53,6 +55,51 @@ router.post("/createItem", async (req, res, next) => {
       next(err)
     }
   })
+
+  
+  // add item to cart
+router.post("/addToCart/:id/:quantity", async (req, res, next) => {
+  try {
+     const id = req.params.id
+     const item = await Store.fetchItemByIdCart(id)
+     const quantity = req.params.quantity
+     if (!item) {
+      const orginalItem = await Store.fetchItemById(id)
+      const item = cloneDeep(orginalItem)
+      item.quantity = req.params.quantity
+      delete item.image
+      delete item.source
+      delete item.description
+      delete item.category
+      const add = await Store.addToCart(item)
+    }
+    else{
+    item.quantity= Number(item.quantity) + Number(quantity)
+    await Store.updateQuantity(item)
+    }
+    
+    //console.log(item)
+    
+     res.status(201).json({ item: item })
+  } catch (err) {
+    next(err)
+  }
+})
+
+ // delete item from cart
+ router.delete("/cart/delete/:id", async (req, res, next) => {
+  try {
+    const listId = req.params.id
+    const item = await Store.fetchItemByIdCart(listId)
+     if (!item) {
+         throw new NotFoundError("Item not found.")
+       }
+    Store.deleteItemFromCart(listId)
+    res.status(200).json("Item "+ listId +" : Deleted Successfully.")
+}catch (err) {
+  next(err)
+}
+})
 
   
 
